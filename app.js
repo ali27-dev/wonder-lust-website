f837e65d1359470827416c37543b0ce71b9664b9
ali27-dev <khanali3229@gmail.com>
2025-09-16 23:32:30 +0500

    i put the functionlity of adding review to listing,I am create post route and put logic in it but there is some thing remaning like validation etc i will add as soon as possible.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
@@ -130,7 +131,20 @@ app.delete(
     res.redirect("/listings");
   })
 );
+////////////////////////////////////////
+////////////Reviews--Post-ROUTE////////////////
+app.post("/listings/:id/reviews", async (req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
 
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  console.log("review was added");
+  res.send("review was added");
+});
 ////////////////////////////////////////
 //////Handling-Error-Middle-wrae///////
 // app.all("*", (req, res, next) => {
