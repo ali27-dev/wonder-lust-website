a45255d4512d36a7cc44018f2c025bc43c1cd3f4
ali27-dev <khanali3229@gmail.com>
2025-09-21 21:51:14 +0500

    post-login page,I'm implement smooth login in project.

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
-
+////Sign-Up-logic
 router.post(
   "/signup",
   wrapAsync(async (req, res) => {
@@ -16,6 +17,7 @@ router.post(
       let newuser = new User({ username, email });
       const registerUser = await User.register(newuser, password);
       console.log(registerUser);
+      // default-login-when sign-up then direct log-in//
       req.login(registerUser, (err) => {
         if (err) {
           return next(err);
@@ -29,22 +31,26 @@ router.post(
     }
   })
 );
-
+////Log-In-logic
 router.get("/login", (req, res) => {
   res.render("users/login.ejs");
 });
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
     req.flash("success", "Welcome back to wonderlust");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
   }
 );
+
+////log-Out-logic
 router.get("/logout", (req, res, next) => {
   req.logOut((err) => {
     if (err) {
