d65f923586368c50f07886f4008465473d7ca851
ali27-dev <khanali3229@gmail.com>
2025-09-21 10:16:54 +0500

    The signup funactionality is complete So go forward and creating login form

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      let newuser = new User({ username, email });
+      const registerUser = await User.register(newuser, password);
+      console.log(registerUser);
+      req.flash("success", "welcome to wonderlust");
+      res.redirect("/listings");
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/signup");
+    }
+  })
+);
 
 module.exports = router;
