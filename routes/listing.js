bb48f63938a36ebb014dea29cf908f85e0a37b8f
ali27-dev <khanali3229@gmail.com>
2025-09-23 11:14:11 +0500

    Uploading Image,First of all i'm added enctype attribute in form and change the image input type to file with yhis we can uploade image from system for this image handling,I'm install multer package,require it and use it, with this it can create upload folder when user add image.

--- routes/listing.js
+++ routes/listing.js
@@ -3,18 +3,23 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwned, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createNewListing)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateListing,
+  //   wrapAsync(listingController.createNewListing)
+  // );
+  .post(upload.single("listing[image]"), (req, res, nex) => {
+    res.send(req.file);
+  });
 
+///////Create: NEW & Create ROUTE///////
 router.get("/new", isLoggedIn, listingController.createNewForm);
 
 router
